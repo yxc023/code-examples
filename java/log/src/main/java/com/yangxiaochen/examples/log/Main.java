dd835b8d984b2496b1a4205eac7a66c45e3cfe97
yxc023 <yxc023@qq.com>
2016-05-11 10:33:44 +0800

    add log examples

---
+++ java/log/src/main/java/com/yangxiaochen/examples/log/Main.java
@@ -0,0 +1,16 @@
+package com.yangxiaochen.examples.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author yangxiaochen
+ * @date 16/5/11 上午12:52
+ */
+public class Main {
+
+    Logger logger  = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        
+    }
+}
