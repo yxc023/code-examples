34019d215a1a611724cc0636983fb9c9d4dd4b8f
yxc023 <yxc023@qq.com>
2016-05-14 22:29:31 +0800

    add json lock

---
+++ java/json/src/main/java/com/yangxiaochen/examples/json/JsonUtils.java
@@ -0,0 +1,23 @@
+package com.yangxiaochen.examples.json;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+/**
+ * @author yangxiaochen
+ * @date 16/5/14 下午9:48
+ */
+public class JsonUtils {
+
+    private static ObjectMapper objectMapper;
+
+    static {
+        objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+    }
+
+    public static String object2String(Object object) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(object);
+    }
+}
