de6051b967af05c4fa5f95a7b10c351d328d5013
yxc023 <yxc023@qq.com>
2016-05-20 22:59:08 +0800

    add log

--- java/activiti-flow/src/main/java/com/yangxiaochen/examples/activiti/listener/MyActivitiEventListener.java
+++ java/activiti-flow/src/main/java/com/yangxiaochen/examples/activiti/listener/MyActivitiEventListener.java
@@ -1,26 +1,34 @@
 package com.yangxiaochen.examples.activiti.listener;
 
+import com.yangxiaochen.examples.activiti.Main;
 import org.activiti.engine.delegate.event.ActivitiEvent;
 import org.activiti.engine.delegate.event.ActivitiEventListener;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * @author yangxiaochen
  * @date 16/4/9 下午5:02
  */
 public class MyActivitiEventListener implements ActivitiEventListener {
+    private static Logger log = LogManager.getLogger(Main.class);
+
     @Override
     public void onEvent(ActivitiEvent event) {
         switch (event.getType()) {
 
             case JOB_EXECUTION_SUCCESS:
                 System.out.println("A job well done!");
+                log.info("A job well done!");
                 break;
 
             case JOB_EXECUTION_FAILURE:
                 System.out.println("A job has failed...");
+                log.info("A job has failed...");
                 break;
 
             default:
+                log.info("Event received: {}", event.getType());
                 System.out.println("Event received: " + event.getType());
         }
 
