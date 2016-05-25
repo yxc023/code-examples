b1f324f0a8f7ecaec7219151074554f60e5698c1
yxc023 <yxc023@qq.com>
2016-05-26 00:36:57 +0800

    up

---
+++ java/bean/src/main/java/com/yangxiaochen/examples/bean/data/Person.java
@@ -0,0 +1,11 @@
+package com.yangxiaochen.examples.bean.data;
+
+import lombok.Data;
+
+/**
+ * @author yangxiaochen
+ * @date 16/5/26 上午12:19
+ */
+public @Data class Person {
+    private int id;
+}
