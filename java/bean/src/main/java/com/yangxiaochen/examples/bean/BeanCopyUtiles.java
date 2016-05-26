86040cc12392d1270ddd8cc8c4ba917802be0c79
yxc023 <yxc023@qq.com>
2016-05-26 23:46:32 +0800

    lombok and  jodd bean

--- java/bean/src/main/java/com/yangxiaochen/examples/bean/BeanCopyUtiles.java
+++ java/bean/src/main/java/com/yangxiaochen/examples/bean/BeanCopyUtiles.java
@@ -1,5 +1,8 @@
 package com.yangxiaochen.examples.bean;
 
+import com.yangxiaochen.examples.bean.data.Person;
+import jodd.bean.BeanCopy;
+
 /**
  * @author yangxiaochen
  * @date 16/5/25 下午11:29
@@ -7,6 +10,11 @@ package com.yangxiaochen.examples.bean;
 public class BeanCopyUtiles {
 
     public static void main(String[] args) {
-        
+        Person p1 = new Person();
+        p1.setId(110);
+
+        Person p2 = new Person();
+        BeanCopy.from(p1).to(p2).copy();
+        System.out.println(p2);
     }
 }
--- java/bean/src/main/java/com/yangxiaochen/examples/bean/data/Person.java
+++ java/bean/src/main/java/com/yangxiaochen/examples/bean/data/Person.java
@@ -1,11 +1,13 @@
 package com.yangxiaochen.examples.bean.data;
 
 import lombok.Data;
+import lombok.ToString;
 
 /**
  * @author yangxiaochen
  * @date 16/5/26 上午12:19
  */
+@ToString
 public @Data class Person {
     private int id;
 }
