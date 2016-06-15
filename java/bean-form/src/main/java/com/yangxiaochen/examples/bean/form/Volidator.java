2661abeca6e8e9b8cdce16cafa3f968b66661f86
yxc023 <yxc023@qq.com>
2016-06-15 15:19:58 +0800

    add hibernate validation

--- java/bean-form/src/main/java/com/yangxiaochen/examples/bean/form/Volidator.java
+++ java/bean-form/src/main/java/com/yangxiaochen/examples/bean/form/Volidator.java
@@ -1,11 +1,28 @@
 package com.yangxiaochen.examples.bean.form;
 
+import com.yangxiaochen.examples.bean.form.forms.ReportForm;
+import lombok.extern.log4j.Log4j2;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
 /**
  * @author yangxiaochen
  * @date 16/6/13 下午5:40
  */
+@Log4j2
 public class Volidator {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+
+        ReportForm reportForm = new ReportForm();
+        Set<ConstraintViolation<ReportForm>> constraintViolations =
+                validator.validate(reportForm);
+        log.info(constraintViolations);
     }
 }
