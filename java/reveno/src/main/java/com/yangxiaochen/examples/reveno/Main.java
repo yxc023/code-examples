570d72cad70e68cd6f8bf558f297d6a0d71d2bf3
yxc023 <yxc023@qq.com>
2016-06-18 01:12:41 +0800

    up

---
+++ java/reveno/src/main/java/com/yangxiaochen/examples/reveno/Main.java
@@ -0,0 +1,46 @@
+package com.yangxiaochen.examples.reveno;
+
+import com.yangxiaochen.examples.reveno.domain.Account;
+import com.yangxiaochen.examples.reveno.domain.AccountView;
+import lombok.extern.log4j.Log4j2;
+import org.reveno.atp.api.Reveno;
+import org.reveno.atp.api.dynamic.DynamicCommand;
+import org.reveno.atp.core.Engine;
+
+import static org.reveno.atp.utils.MapUtils.map;
+
+/**
+ * @author yangxiaochen
+ * @date 16/6/17 下午5:53
+ */
+@Log4j2
+public class Main {
+    public static void main(String[] args) {
+        Reveno reveno = new Engine("/tmp/reveno-sample");
+
+        reveno.domain().viewMapper(Account.class, AccountView.class,
+                (id, e, r) -> new AccountView(id, e.name, e.balance));
+
+        DynamicCommand createAccount = reveno.domain()
+                .transaction("createAccount", (t, c) -> c.repo().store(t.id(), new Account(t.arg(), 0)))
+                .uniqueIdFor(Account.class).command();
+
+        DynamicCommand changeBalance = reveno.domain()
+                .transaction("changeBalance", (t, c) -> c.repo().store(t.longArg(), c.repo().get(Account.class, t.arg()).add(t.intArg("inc"))))
+                .command();
+
+
+        reveno.startup();
+
+        long accountId = reveno.executeSync(createAccount, map("name", "John"));
+        reveno.executeSync(changeBalance, map("id", accountId, "inc", 10_000));
+
+        log.info("{}, {}", reveno.query().find(AccountView.class, accountId));
+        log.info("{}, {}", reveno.query().find(AccountView.class, accountId).name, "John");
+        log.info("{}, {}", reveno.query().find(AccountView.class, accountId).balance, 10000);
+
+        reveno.shutdown();
+
+
+    }
+}
---
+++ java/reveno/src/main/java/com/yangxiaochen/examples/reveno/domain/AccountView.java
@@ -0,0 +1,13 @@
+package com.yangxiaochen.examples.reveno.domain;
+
+public class AccountView {
+	public final long id;
+	public final String name;
+	public final long balance;
+
+	public AccountView(long id, String name, long balance) {
+		this.id = id;
+		this.name = name;
+		this.balance = balance;
+	}
+}
