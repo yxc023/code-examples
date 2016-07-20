cebe13b86303d1352bb9b86073acbb216702d3ea
yxc023 <yxc023@qq.com>
2016-07-20 23:42:21 +0800

    up

---
+++ java/bean/src/test/java/com/yangxiaochen/examples/bean/CopyTest.java
@@ -0,0 +1,14 @@
+package com.yangxiaochen.examples.bean;
+
+import org.junit.Test;
+
+/**
+ * @author yangxiaochen
+ * @date 16/7/20 下午11:41
+ */
+public class CopyTest {
+    @Test
+    public void copyTest() {
+
+    }
+}
