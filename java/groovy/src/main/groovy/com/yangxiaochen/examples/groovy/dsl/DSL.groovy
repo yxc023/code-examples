44be89fcec82d5f1a749347d4e67eba299dbcfee
yxc023 <yxc023@qq.com>
2016-07-31 22:56:52 +0800

    dsl

---
+++ java/groovy/src/main/groovy/com/yangxiaochen/examples/groovy/dsl/DSL.groovy
@@ -0,0 +1,25 @@
+show = { println it }
+square_root = { Math.sqrt(it) }
+
+def please(action) {
+    [the: { what ->
+        [of: { n -> action(what(n)) }]
+    }
+    ]
+}
+
+// equivalent to: please(show).the(square_root).of(100)
+please show the square_root of 100
+
+
+def split(String string) {
+    [on: { sp ->
+        [trimming: { trim ->
+           string.split sp collect{ it.trim().replaceAll(trim, "") }
+        }]
+    }]
+}
+
+
+def result = split "_a ,_b_ ,c__" on ',' trimming '_'
+println result
