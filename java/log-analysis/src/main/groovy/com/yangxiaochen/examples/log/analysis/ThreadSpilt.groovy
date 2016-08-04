1e72c0d47259d3cd4f136f15da44cc12a629cba4
yxc023 <yxc023@qq.com>
2016-08-04 22:06:16 +0800

    log analysis

---
+++ java/log-analysis/src/main/groovy/com/yangxiaochen/examples/log/analysis/ThreadSpilt.groovy
@@ -0,0 +1,30 @@
+package com.yangxiaochen.examples.log.analysis
+/**
+ * @author yangxiaochen
+ * @date 16/8/4 下午5:49
+ */
+class ThreadSpilt {
+
+    public static void main(String[] args) {
+
+        def fileMap = [:]
+        File logFile = new File("/Users/yangxiaochen/Downloads/deadlock.log")
+        logFile.eachLine { line ->
+            def terms = line.split(" ")
+            if (terms.length <= 3) {
+                return
+            }
+            if ((terms[0] + " " + terms[1]).matches(/^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}$/)) {
+                def threadName = terms[2][1..-2]
+                if (!fileMap.containsKey(threadName)) {
+
+                    fileMap["${threadName}"] = new File(logFile.getPath() + threadName)
+                }
+
+                fileMap["${threadName}"] << line << "\n"
+
+            }
+        }
+
+    }
+}
