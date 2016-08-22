eb11ee30faafcbb22c878cbb11f3c07a66eec85d
yxc023 <yxc023@qq.com>
2016-08-23 01:02:34 +0800

    kotlin

---
+++ java/kotlin/src/main/kotlin/com/yangxiaochen/examples/kotlin/BasicSyntax.kt
@@ -0,0 +1,27 @@
+package com.yangxiaochen.examples.kotlin
+
+/**
+ * @author yangxiaochen
+ * @date 16/8/23 上午12:53
+ */
+fun add(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun sum(a: Int, b: Int) = a + b
+
+
+val a: Int = 1
+
+
+fun main(args: Array<String>) {
+    if (args.size == 0) return
+
+    print("First argument: ${args[0]}")
+}
+
+
+fun parseInt(str: String): Int? {
+    return null
+}
+
