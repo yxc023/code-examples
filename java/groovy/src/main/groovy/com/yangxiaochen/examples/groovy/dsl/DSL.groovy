521bb2147691974a48cf72dac51dce72a55149c7
yxc023 <yxc023@qq.com>
2016-08-30 22:37:11 +0800

    up

--- java/groovy/src/main/groovy/com/yangxiaochen/examples/groovy/dsl/DSL.groovy
+++ java/groovy/src/main/groovy/com/yangxiaochen/examples/groovy/dsl/DSL.groovy
@@ -15,7 +15,7 @@ please show the square_root of 100
 def split(String string) {
     [on: { sp ->
         [trimming: { trim ->
-           string.split sp collect{ it.trim().replaceAll(trim, "") }
+            string.split sp collect { it.trim().replaceAll(trim, "") }
         }]
     }]
 }
@@ -32,3 +32,29 @@ def split(String string) {
 
 def result = split "_a ,_b_ ,c__" on ',' trimming '_'
 println result
+
+
+
+
+
+biz("天津面签") {
+    desc = "天津面签"
+    participants = [
+            {
+                name = "JIAO_YI_ZHU_BAN"
+                desc = "交易主办"
+            },
+            {
+                name = "ZHU_BAN_ZHU_LI"
+                desc = "主办助理"
+                fenDan = true
+            }]
+    attachments = []
+    variables = [
+            {
+                name = "MIAN_QIAN_YIN_HANG_bankName"
+                desc = "面签银行总行"
+                dataType = "String"
+            }]
+    variableGroups = []
+}
