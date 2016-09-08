3c14c027800b0c53f4342d95ce88d85286b852eb
yxc023 <yxc023@qq.com>
2016-09-08 17:08:30 +0800

    up

--- java/groovy/src/main/groovy/com/yangxiaochen/examples/groovy/dsl/DSL.groovy
+++ java/groovy/src/main/groovy/com/yangxiaochen/examples/groovy/dsl/DSL.groovy
@@ -37,31 +37,3 @@ println result
 
 
 
-biz {
-    desc = "天津面签"
-    participants = [
-            {
-                name = "JIAO_YI_ZHU_BAN"
-                desc = "交易主办"
-            },
-            {
-                name = "ZHU_BAN_ZHU_LI"
-                desc = "主办助理"
-                fenDan = true
-            }]
-    attachments = []
-    variables = [
-            {
-                name = "MIAN_QIAN_YIN_HANG_bankName"
-                desc = "面签银行总行"
-                dataType = "String"
-            }]
-    variableGroups = []
-}
-
-
-
-class BizObj {
-    def desc
-}
-def biz = new BizObj()
---
+++ java/groovy/src/main/groovy/com/yangxiaochen/examples/groovy/dsl/MyDsL.groovy
@@ -0,0 +1,54 @@
+package com.yangxiaochen.examples.groovy.dsl
+
+/**
+ * @author yangxiaochen
+ * @date 16/9/7 上午1:25
+ */
+biz("天津面签") {
+    desc = "天津面签"
+    participants = [
+            {
+                name = "JIAO_YI_ZHU_BAN"
+                desc = "交易主办"
+            },
+            {
+                name = "ZHU_BAN_ZHU_LI"
+                desc = "主办助理"
+                fenDan = true
+            }]
+    attachments = []
+    variables = [
+            {
+                name = "MIAN_QIAN_YIN_HANG_bankName"
+                desc = "面签银行总行"
+                dataType = "String"
+            }]
+    variableGroups = []
+}
+
+class L {
+    String name
+    String desc
+}
+class BizObj {
+    String desc
+    List participants
+    List attachments
+    List variables
+    List variableGroups
+
+    def call(){
+        println "call"
+    }
+}
+
+def biz(name, @DelegatesTo(value = BizObj, strategy = Closure.DELEGATE_ONLY)Closure closure) {
+    println name
+    def bodySpec = new BizObj()
+    def code = closure.rehydrate(bodySpec, this, this)
+    code.resolveStrategy = Closure.DELEGATE_ONLY
+    println code.delegate.dump()
+
+    code.call()
+    println code.delegate.dump()
+}
