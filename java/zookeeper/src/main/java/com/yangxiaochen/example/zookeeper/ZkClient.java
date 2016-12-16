76ba0c628e068e652a2f82e63ca4ed76f6c72d25
yxc023 <yxc023@qq.com>
2016-12-16 18:38:09 +0800

    simple zookeeper test

--- java/zookeeper/src/main/java/com/yangxiaochen/example/zookeeper/ZkClient.java
+++ java/zookeeper/src/main/java/com/yangxiaochen/example/zookeeper/ZkClient.java
@@ -1,6 +1,7 @@
 package com.yangxiaochen.example.zookeeper;
 
 import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
 import java.io.IOException;
@@ -14,47 +15,54 @@ public class ZkClient {
     ZooKeeper zk;
 
 
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
+        String znode = "/zktest1";
         ZkClient zkClient = new ZkClient();
-        zkClient.zk = new ZooKeeper("127.0.0.1:2181", 3000, event -> {
+        zkClient.zk = new ZooKeeper("127.0.0.1:2181,127.0.0.1:2182,127.0.0.1:2183", 3000, event -> {
             System.out.println(event);
-            try {
-               zkClient.zk.exists("/zktest1",true);
 
-//                System.out.println(new String(data));
-            } catch (KeeperException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            if (event.getType() == Watcher.Event.EventType.None) {
+                // We are are being told that the state of the
+                // connection has changed
+                switch (event.getState()) {
+                    case SyncConnected:
+                        // In this particular example we don't need to do anything
+                        // here - watches are automatically re-registered with
+                        // server and any watches triggered while the client was
+                        // disconnected will be delivered (in order of course)
+                        break;
+                    case Expired:
+                        /** The serving cluster has expired this session. The ZooKeeper
+                         * client connection (the session) is no longer valid. You must
+                         * create a new client connection (instantiate a new ZooKeeper
+                         * instance) if you with to access the ensemble. */
+                        return;
+
+                }
+            } else {
+
+                try {
+                    zkClient.zk.exists(znode, true);
+                } catch (KeeperException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
 
+        while (true) {
+            if (zkClient.zk.getState().equals(ZooKeeper.States.CONNECTED)) {
+                break;
+            }
+            Thread.yield();
+        }
+        zkClient.zk.exists(znode, true);
+        zkClient.zk.register((event) -> {
+            System.out.println("i'm the watch2~ " + event);
+        });
 
-//        AsyncCallback.StatCallback callback = new AsyncCallback.StatCallback() {
-//            @Override
-//            public void processResult(int rc, String path, Object ctx, Stat stat) {
-////                zk.exists("/zktest", true, this, null);
-//                if (rc == KeeperException.Code.Ok) {
-//                    try {
-//                        byte[] data = zk.getData("/zktest",true,null);
-//                        System.out.println(new String(data));
-//                    } catch (KeeperException e) {
-//                        e.printStackTrace();
-//                    } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//                System.out.println(rc + " " + path + " " + ctx + " " + stat);
-////                zk.exists("/zktest", true, (r,p,c,s)->{
-////                    System.out.println(r + " " + p + " " + c + " " + s);
-////                }, null);
-//            }
-//        };
-
-
-//        zk.exists("/zktest", true, callback, null);
 
         System.in.read();
     }
