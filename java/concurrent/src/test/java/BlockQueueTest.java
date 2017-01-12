f428ff1f7145afa2da76de851296b2c82f029336
yxc023 <yxc023@qq.com>
2017-01-12 12:23:00 +0800

    add concurrent book source

---
+++ java/concurrent/src/test/java/BlockQueueTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * @author yangxiaochen
+ * @date 2017/1/12 12:16
+ */
+public class BlockQueueTest {
+
+    @Test
+    public void testBlockQueue() {
+        LinkedBlockingQueue queue = new LinkedBlockingQueue();
+
+    }
+}
