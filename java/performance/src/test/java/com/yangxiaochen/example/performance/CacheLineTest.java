e9d9e9a30f5db3861ba332150f1f367fe9556f78
yxc023 <yxc023@qq.com>
2017-01-22 17:40:03 +0800

    up

---
+++ java/performance/src/test/java/com/yangxiaochen/example/performance/CacheLineTest.java
@@ -0,0 +1,38 @@
+package com.yangxiaochen.example.performance;
+
+import org.junit.Test;
+
+/**
+ * @author yangxiaochen
+ * @date 2017/1/22 17:05
+ */
+public class CacheLineTest {
+
+    @Test
+    public void test() {
+        long[][] arr;
+        arr = new long[1024 * 1024][];
+        for (int i = 0; i < 1024 * 1024; i++) {
+            arr[i] = new long[8];
+            for (int j = 0; j < 8; j++) {
+                arr[i][j] = 0L;
+            }
+        }
+        long sum = 0L;
+        long marked = System.currentTimeMillis();
+        for (int i = 0; i < 1024 * 1024; i+=1) {
+            for(int j =0; j< 8;j++){
+                sum = arr[i][j];
+            }
+        }
+        System.out.println("Loop times:" + (System.currentTimeMillis() - marked) + "ms");
+
+        marked = System.currentTimeMillis();
+        for (int i = 0; i < 8; i+=1) {
+            for(int j =0; j< 1024 * 1024;j++){
+                sum = arr[j][i];
+            }
+        }
+        System.out.println("Loop times:" + (System.currentTimeMillis() - marked) + "ms");
+    }
+}
