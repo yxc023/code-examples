8a3356bad33941bbfd5444419e5a0c387d23d63a
yxc023 <yxc023@qq.com>
2017-04-11 11:22:17 +0800

    instance inner class

--- java/inner-class/src/main/java/com/yangxiaochen/example/inner/Printer.java
+++ java/inner-class/src/main/java/com/yangxiaochen/example/inner/Printer.java
@@ -1,20 +1,29 @@
 package com.yangxiaochen.example.inner;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * @author yangxiaochen
  * @date 2017/4/11 10:34
  */
 public class Printer {
 
-    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
         OuterClass outerClass = new OuterClass();
 
-//        System.out.println(outerClass.createFeildClass().getClass());
-//        System.out.println(outerClass.createInner().getClass());
+        System.out.println(outerClass.createFeildClass().getClass());
+        System.out.println(outerClass.createInner().getClass());
+
 
 
         Class z = Class.forName("com.yangxiaochen.example.inner.OuterClass$2");
-        Object o = z.newInstance();
+        // 内部类的构造方法第一个参数是持有它的类
+        for (Constructor constructor : z.getDeclaredConstructors()) {
+            System.out.println(constructor);
+        }
+
+        Object o = z.getDeclaredConstructor(OuterClass.class).newInstance(outerClass);
         OuterClass oc = (OuterClass) o;
         oc.action();
     }
