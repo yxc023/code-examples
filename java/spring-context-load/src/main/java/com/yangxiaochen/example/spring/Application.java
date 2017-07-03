ebddca39a957e093be610aa2ceb3bdaf1e489dd4
yxc023 <yxc023@qq.com>
2017-07-03 10:41:16 +0800

    up

--- java/spring-context-load/src/main/java/com/yangxiaochen/example/spring/Application.java
+++ java/spring-context-load/src/main/java/com/yangxiaochen/example/spring/Application.java
@@ -3,12 +3,14 @@ package com.yangxiaochen.example.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * @author yangxiaochen
  * @date 2017/3/6 23:37
  */
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class Application {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
