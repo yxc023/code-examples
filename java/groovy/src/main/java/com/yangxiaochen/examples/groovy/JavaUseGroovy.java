a24eceb461f4a0b996547a7bca5bac0b73f5df85
yxc023 <yxc023@qq.com>
2017-08-02 00:34:54 +0800

    up

--- java/groovy/src/main/java/com/yangxiaochen/examples/groovy/JavaUseGroovy.java
+++ java/groovy/src/main/java/com/yangxiaochen/examples/groovy/JavaUseGroovy.java
@@ -3,11 +3,8 @@ package com.yangxiaochen.examples.groovy;
 import groovy.lang.GroovyShell;
 import groovy.lang.Script;
 import org.codehaus.groovy.control.CompilerConfiguration;
-import org.codehaus.groovy.reflection.ClassInfo;
-import org.codehaus.groovy.reflection.GroovyClassValue;
 
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.util.Date;
 
 /**
@@ -16,7 +13,10 @@ import java.util.Date;
  */
 public class JavaUseGroovy {
     public static void main(String[] args) throws IOException, NoSuchFieldException, IllegalAccessException {
-
+//        CompilerConfiguration configuration = new CompilerConfiguration();
+//        configuration.setScriptBaseClass("com.yangxiaochen.examples.groovy.ScriptResolver");
+//        configuration.setRecompileGroovySource(false);
+//        GroovyShell groovyShell = new GroovyShell(JavaUseGroovy.class.getClassLoader(), configuration);
         for (int i = 0; i < 1000000; i++) {
             CompilerConfiguration configuration = new CompilerConfiguration();
             configuration.setScriptBaseClass("com.yangxiaochen.examples.groovy.ScriptResolver");
@@ -29,11 +29,15 @@ public class JavaUseGroovy {
             Script s2 = groovyShell.parse("print \"now is ${date}\"");
             s2.run();
 
-            Field globalClassValue = ClassInfo.class.getDeclaredField("globalClassValue");
-            globalClassValue.setAccessible(true);
-            GroovyClassValue classValueBean = (GroovyClassValue) globalClassValue.get(null);
-            classValueBean.remove(s1.getClass());
-            classValueBean.remove(s2.getClass());
+//            groovyShell.getClassLoader().clearCache();
+
+//            Field globalClassValue = ClassInfo.class.getDeclaredField("globalClassValue");
+//            globalClassValue.setAccessible(true);
+//            GroovyClassValue classValueBean = (GroovyClassValue) globalClassValue.get(null);
+//            classValueBean.remove(s1.getClass());
+//            classValueBean.remove(s2.getClass());
+//            ClassInfo.remove(s1.getClass());
+//            ClassInfo.remove(s2.getClass());
 
         }
     }
