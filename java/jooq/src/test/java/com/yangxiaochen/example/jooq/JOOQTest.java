5054de5ea5a687c3b84cd68c22bf3f3b091c0c9c
yxc023 <yxc023@qq.com>
2018-04-16 15:10:36 +0800

    up

--- java/jooq/src/test/java/com/yangxiaochen/example/jooq/JOOQTest.java
+++ java/jooq/src/test/java/com/yangxiaochen/example/jooq/JOOQTest.java
@@ -1,6 +1,8 @@
 package com.yangxiaochen.example.jooq;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import com.yangxiaochen.jooq.tables.daos.BookDao;
+import com.yangxiaochen.jooq.tables.pojos.Book;
 import com.yangxiaochen.jooq.tables.records.BookRecord;
 import org.jooq.*;
 import org.jooq.impl.DSL;
@@ -12,7 +14,6 @@ import java.util.concurrent.CompletionStage;
 
 import static com.yangxiaochen.jooq.Tables.BOOK;
 import static com.yangxiaochen.jooq.Tables.LANGUAGE;
-import static org.jooq.impl.DSL.using;
 
 /**
  * @author yangxiaochen
@@ -27,8 +28,7 @@ public class JOOQTest {
         mysqlDataSource.setDatabaseName("jooq_test");
         mysqlDataSource.setUser("root");
         mysqlDataSource.setPassword("");
-
-        sql = using(mysqlDataSource, SQLDialect.MYSQL);
+        sql = DSL.using(mysqlDataSource, SQLDialect.MYSQL);
     }
 
     @Test
@@ -40,19 +40,19 @@ public class JOOQTest {
         }
 
         {
-            String s = DSL.select()
+            String s = DSL.using(SQLDialect.MYSQL).select()
                     .from("book")
-                    .join(DSL.table("language").as("l"))
-                    .on("bool.language_id = l.id")
+                    .join(DSL.table("language").as("a"))
+                    .on("book.language_id = a.id")
                     .getSQL();
             System.out.println(s);
         }
 
         {
-            String s = DSL.select()
+            String s = DSL.using(SQLDialect.MYSQL).select()
                     .from("book")
-                    .join(DSL.table("language").as("l"))
-                    .on("bool.language_id = l.id")
+                    .join(DSL.table("language").as("a"))
+                    .on("book.language_id = a.id")
                     .where(DSL.field("book.author_id").eq(2))
                     .and("published_in = 1990")
                     .getSQL();
@@ -65,40 +65,77 @@ public class JOOQTest {
     @Test
     public void asSqlExecutor() {
         {
-            Result<Record3<Object, Object, Object>> result = sql.select(DSL.field("id"), DSL.field("title"), DSL.field("published_in"))
+            String s = DSL.using(SQLDialect.MYSQL).select()
                     .from("book")
-                    .fetch();
-            System.out.println(result);
-            System.out.println(result.get(0).value1());
-            System.out.println(result.get(0).value1().getClass());
-            System.out.println(result.get(0).component2());
+                    .join(DSL.table("language").as("a"))
+                    .on("book.language_id = a.id")
+                    .where(DSL.field("book.author_id").eq(2))
+                    .and("published_in = 1990")
+                    .getSQL();
+            sql.execute(s, 2);
+            sql.resultQuery(s, 2).fetch();
         }
-
-
-        Result<Record3<Integer, Object, Object>> result2 = sql.select(DSL.field("id", Integer.class), DSL.field("title"), DSL.field("published_in"))
-                .from("book")
-                .fetch();
-        System.out.println(result2);
-        System.out.println(result2.get(0).value1());
-        System.out.println(result2.get(0).value1().getClass());
-        System.out.println(result2.get(0).component2());
-
-
         {
-            Result<Record3<Object, Object, Object>> result = sql.select(DSL.field("id"), DSL.field("title"), DSL.field("published_in"))
+            SelectQuery query = DSL.using(SQLDialect.MYSQL).select()
                     .from("book")
-                    .where(DSL.field("published_in").eq("1990"))
-                    .fetch();
-            System.out.println(result);
-            System.out.println(result.get(0).value1());
-            System.out.println(result.get(0).value1().getClass());
-            System.out.println(result.get(0).component2());
+                    .join(DSL.table("language").as("a"))
+                    .on("book.language_id = a.id")
+                    .where(DSL.field("book.author_id").eq(2))
+                    .and("published_in = 1990")
+                    .getQuery();
+            query.bind(1, 2);
+            query.attach(sql.configuration());
+            query.fetch();
+
         }
+//        {
+//            Result<Record3<Object, Object, Object>> result = sql.select(DSL.field("id"), DSL.field("title"), DSL.field("published_in"))
+//                    .from("book")
+//                    .fetch();
+//            System.out.println(result);
+//            System.out.println(result.get(0).value1());
+//            System.out.println(result.get(0).value1().getClass());
+//            System.out.println(result.get(0).component2());
+//        }
+//
+//
+//        Result<Record3<Integer, Object, Object>> result2 = sql.select(DSL.field("id", Integer.class), DSL.field("title"), DSL.field("published_in"))
+//                .from("book")
+//                .fetch();
+//        System.out.println(result2);
+//        System.out.println(result2.get(0).value1());
+//        System.out.println(result2.get(0).value1().getClass());
+//        System.out.println(result2.get(0).component2());
+//
+//
+//        {
+//            Result<Record3<Object, Object, Object>> result = sql.select(DSL.field("id"), DSL.field("title"), DSL.field("published_in"))
+//                    .from("book")
+//                    .where(DSL.field("published_in").eq("1990"))
+//                    .fetch();
+//            System.out.println(result);
+//            System.out.println(result.get(0).value1());
+//            System.out.println(result.get(0).value1().getClass());
+//            System.out.println(result.get(0).component2());
+//        }
     }
 
 
     @Test
     public void typesafeSql() {
+
+        {
+            Result<Record3<Integer, Integer, String>> result = sql.select(BOOK.ID, BOOK.AUTHOR_ID, LANGUAGE.DESCRIPTION).from(BOOK).join(LANGUAGE)
+                    .on(BOOK.LANGUAGE_ID.eq(LANGUAGE.ID))
+                    .where(BOOK.PUBLISHED_IN.eq(1990))
+                    .fetch();
+            Integer id = result.get(0).getValue(BOOK.ID);
+            Integer authorId = result.get(0).get("author_id", Integer.class);
+            String desc = result.get(0).value3();
+            System.out.println(id + " " + authorId + " " + desc);
+
+        }
+
         SelectQuery query = sql.select(BOOK.ID, BOOK.AUTHOR_ID, LANGUAGE.DESCRIPTION).from(BOOK).join(LANGUAGE)
                 .on(BOOK.LANGUAGE_ID.eq(LANGUAGE.ID))
                 .where(BOOK.PUBLISHED_IN.eq(1990))
@@ -133,11 +170,14 @@ public class JOOQTest {
         bookRecord1.setAuthorId(2);
         bookRecord1.setLanguageId(1);
         bookRecord1.setTitle("hello");
-//        bookRecord1.setId(10);
         bookRecord1.setPublishedIn(2018);
         bookRecord1.store();
 
         System.out.println(bookRecord1);
+
+        BookDao bookDao = new BookDao(sql.configuration());
+        List<Book> books = bookDao.fetchByAuthorId(2);
+        System.out.println(books.get(0));
     }
 
 
@@ -161,16 +201,15 @@ public class JOOQTest {
                     '}';
         }
     }
+
     @Test
-    public void map() {
+    public void mapping() {
 
         List<R> r = sql.select(BOOK.ID, BOOK.AUTHOR_ID, LANGUAGE.DESCRIPTION).from(BOOK).join(LANGUAGE)
                 .on(BOOK.LANGUAGE_ID.eq(LANGUAGE.ID))
                 .where(BOOK.PUBLISHED_IN.eq(1990)).fetch().into(R.class);
 
-
         r.forEach(it -> System.out.println(it));
-
     }
 
 
